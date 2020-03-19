28ee99a4d2b0d7964bcdceedd109b1d42c6878aa
MtDeity <o8olo853q43@gmail.com>
2020-03-19 17:07:18 +0900

    routes設定

---
+++ lib/screens/training_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrainingScreen extends StatelessWidget {
+  static const String id = 'training_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
